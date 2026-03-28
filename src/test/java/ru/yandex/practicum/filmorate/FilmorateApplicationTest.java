9ec0ca697cc2cf85040a612dfaa35d0137dc7392
Алина <yyasenu@mail.ru>
2026-03-28 22:32:37 +0300

    исправление ошибки

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTest.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FilmorateApplicationTests {
+class FilmorateApplicationTest {
 
     private final UserController userController = new UserController();
     private final FilmController filmController = new FilmController();
