26b135328e5f0deaecea69c833f470c74939b30b
Алина <yyasenu@mail.ru>
2026-04-14 14:31:56 +0300

    ..

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@Valid @RequestBody User user) {
+    public User createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         log.info("Создан пользователь: {}", createdUser.getLogin());
         return createdUser;
