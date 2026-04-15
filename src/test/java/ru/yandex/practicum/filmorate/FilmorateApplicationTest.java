bcca65653142fe18e598b41e80f41e29c409b8fa
Алина <yyasenu@mail.ru>
2026-04-15 12:29:31 +0300

    ;;

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
