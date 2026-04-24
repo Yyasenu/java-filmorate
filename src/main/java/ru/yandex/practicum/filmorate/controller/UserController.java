9b822a9dab4835736cfeaaa84094fddd8eed7a11
Алина <yyasenu@mail.ru>
2026-04-24 16:33:13 +0300

    11 спринт

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
